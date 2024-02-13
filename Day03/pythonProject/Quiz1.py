572145366ea01569337c983f1a51050c548d1ef6
kimbj829 <49744538+kimbj829@users.noreply.github.com>
2024-02-13 13:03:36 +0900

    Delete Day03 directory

--- Day03/pythonProject/Quiz1.py
+++
@@ -1,6 +0,0 @@
-'''
-PYTHON 이라는 문자열을 거꾸로 뒤집어 출력
-'''
-
-string = "PYTHON";
-print(string[::-1]);
--- Day03/pythonProject/ex07.py
+++
@@ -1,4 +0,0 @@
-num1 = int(input("숫자입력 >> "));
-num2 = int(input("숫자입력 >> "));
-
-print("두 수의 덧셈 결과 :", num1 + num2);
